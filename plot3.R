6ef6523299f7198352955544aff8c9fe3bb5ec0b
colleen-davies <colleendavies@icloud.com>
2021-03-04 18:57:00 -0700

    removed a lines command by using type = "l" instead

--- plot3.R
+++ plot3.R
@@ -30,9 +30,8 @@ HPCdfsub$DateTime <- strptime(HPCdfsub$DateTime, format = "%Y-%m-%d %H:%M:%S") #
 png(filename = "plot3.png", width = 480, height = 480, units = "px", bg = "transparent")
 
 # Constructing the plot
-plot(HPCdfsub$DateTime, HPCdfsub$Sub_metering_1, type = "n", xlab = "", 
-     ylab = "Energy sub metering")
-lines(HPCdfsub$DateTime, HPCdfsub$Sub_metering_1, col = "black")
+plot(HPCdfsub$DateTime, HPCdfsub$Sub_metering_1, type = "l", xlab = "", 
+     ylab = "Energy sub metering", col = "black")
 lines(HPCdfsub$DateTime, HPCdfsub$Sub_metering_2, col = "red")
 lines(HPCdfsub$DateTime, HPCdfsub$Sub_metering_3, col = "blue")
 legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
